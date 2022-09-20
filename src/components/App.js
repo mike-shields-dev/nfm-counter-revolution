879c535f11415c1c2fc4b78b3c5476ae1bc14b1d
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-20 11:29:48 +0100

    feat: Add heading to App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1>Counter Revolution</h1>
+    </div>
+  );
 };
 
 export default App;
