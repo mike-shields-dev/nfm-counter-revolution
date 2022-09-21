e3764447d31b08761fe81f7ec80065cb51ed3f10
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-21 09:15:35 +0100

    feat: App displays a heading, content container and a revolver container

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+
+describe("App", () => {
+  it("matches snapshot", () => {
+    const { asFragment } = render(<App />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders a heading", () => {
+    render(<App />);
+
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+  });
+
+  it("renders a the content area", () => {
+    render(<App />);
+
+    expect(screen.getByText(/content/i)).toBeInTheDocument();
+  });
+
+  it("renders a the revolver area", () => {
+    render(<App />);
+
+    expect(screen.getByText(/revolver/i)).toBeInTheDocument();
+  });
+});
--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,14 @@ import "../styles/App.css";
 
 const App = () => {
   return (
-    <div className="App" data-testid="app">
-      <h1>Counter Revolution</h1>
+    <div className="App">
+      <header>
+        <h1>Counter Revolution</h1>
+      </header>
+      <main>
+        <div className="content">Content</div>
+        <div className="revolver">Revolver</div>
+      </main>
     </div>
   );
 };
