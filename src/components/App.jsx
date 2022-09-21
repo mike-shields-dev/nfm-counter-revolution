bf5c7971b719da78efe56cd2d36f31b40b4e4e28
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-21 10:22:19 +0100

    feat: Add state to App component
    - Adds useState hook of index
    - Adds currentFigure constant derived from figures data and index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import figures from "../data.json";
 import Revolver from "./Revolver";
 
 const App = () => {
-  // const [index, setIndex] = useState(0);
-  // const currentFigure = figures[index];
+  const [index, setIndex] = useState(0);
+  const currentFigure = figures[index];
 
-  // const incrementIndex = () =>
-  //   setIndex((prev) => (prev < figures.length - 1 ? prev + 1 : 0));
+  const incrementIndex = () =>
+    setIndex((prev) => (prev < figures.length - 1 ? prev + 1 : 0));
 
   return (
     <div className="App">
       <header>
-        <h1>title</h1>
+        <h1>{currentFigure.title}</h1>
       </header>
       <main>
         <div className="content" data-testid="content">
-          Content
+          {currentFigure.text}
         </div>
-        <Revolver />
+        <Revolver incrementIndex={incrementIndex} />
       </main>
     </div>
   );
