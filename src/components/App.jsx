7f744647c112d6bf93928b26a3dfe40d0e3155bc
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-21 10:17:17 +0100

    feat: Import dummy data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "../styles/App.css";
+import figures from "../data.json";
 import Revolver from "./Revolver";
 
 const App = () => {
+  // const [index, setIndex] = useState(0);
+  // const currentFigure = figures[index];
+
+  // const incrementIndex = () =>
+  //   setIndex((prev) => (prev < figures.length - 1 ? prev + 1 : 0));
+
   return (
     <div className="App">
       <header>
